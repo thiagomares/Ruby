b9f8296c410061351cee9894f9051f868c838f8b
thiagomares <39134260+thiagomares@users.noreply.github.com>
2021-02-06 20:44:50 -0300

    Recursividade

---
+++ Classe.py
@@ -0,0 +1,7 @@
+class Pythonito():
+    def Teste(*Valores):
+        print("Olá mundo")
+        print(Valores)
+        print(type(Valores))
+
+Pythonito.Teste({1, 251, 5, 7, 9})
---
+++ Recursividade.rb
@@ -0,0 +1,23 @@
+=begin
+    Funções Recursivas
+
+    Recursividade é o ato de chamar o mesmo de entrada várias vezes entrando em looping
+
+    Quando estamos utilizando recursividade, devemos pensar em formas de sair dele, pois ele tende ao infinito
+=end
+
+
+require 'byebug' # O byebug permite que nos possamos ver onde está o erro no codigo
+require 'rails'
+
+Lista = []
+
+def Recursividade(contador = 0)
+    puts "Thiago"
+    Lista[Lista.length] = contador
+    return if contador === 10
+    Recursividade(contador+=1)
+end
+
+Recursividade()
+puts Lista.class
