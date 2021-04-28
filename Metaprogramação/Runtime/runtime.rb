58157bf8fe78a8e5800de7d9f4922e5ebef61a45
thiagomares <thiago.mares4@gmail.com>
2021-04-28 18:38:00 -0300

    Runtime

---
+++ Metaprogramação/Runtime/runtime.rb
@@ -0,0 +1,9 @@
+# Metaprogramação - Runtime
+
+=begin 
+    Para gente começar, o que é metaprogramação?
+
+    Metaprogramação é a forma que um código manipula outros codigos ou a si mesmo
+
+    
+=end
