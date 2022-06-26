0043775373ab1f74a1bdfa12beb0d592ac04ab76
thiagomares <thiago.mares4@gmail.com>
2022-06-26 18:06:27 -0300

    main

---
+++ Poliformismo/polimorfismo.rb
@@ -0,0 +1,23 @@
+=begin
+    Polimorfismo e overload
+
+    Polimorfismo é a caracteristica de escrever o mesmo método de várias formas
+
+    
+=end
+require 'byebug'
+
+
+require_relative 'carro'
+require_relative 'Fiesta'
+require_relative 'golf'
+
+golf = Golf.new
+carro = Carro.new
+fiesta = Fiesta.new
+
+fiesta.cor = "branco"
+carro.modelo = "Fiesta"
+fiesta.modelo = 'Fiesta'
+fiesta.mostrar
+puts fiesta.show
